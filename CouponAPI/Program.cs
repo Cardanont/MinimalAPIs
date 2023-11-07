116f0328842682fb590717fdc1392dd7fe1808ca
DanielO <11862549+Cardanont@users.noreply.github.com>
2023-11-06 23:01:27 -0600

    Adding DTOs

--- CouponAPI/Program.cs
+++ CouponAPI/Program.cs
@@ -1,5 +1,6 @@
 using CouponAPI.Data;
 using CouponAPI.Models;
+using CouponAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,21 +36,36 @@ app.MapGet("/api/coupon/{id:int}", (int id) =>
 
 }).WithName("GetCoupon").Produces<Coupon>(200);
 
-app.MapPost("/api/coupon", ([FromBody]Coupon coupon) =>
+app.MapPost("/api/coupon", ([FromBody]CouponCreateDTO coupon_C_DTO) =>
 {
-    if(coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+    if(string.IsNullOrEmpty(coupon_C_DTO.Name))
     {
         return Results.BadRequest("Invalid Id or coupon Name");
     }
-    if(CouponStore.couponList.FirstOrDefault(c => c.Name.ToLower() == coupon.Name.ToLower()) != null)
+    if(CouponStore.couponList.FirstOrDefault(c => c.Name.ToLower() == coupon_C_DTO.Name.ToLower()) != null)
     {
         return Results.BadRequest("Coupon Name already exists");
     }
 
+    Coupon coupon = new()
+    {
+        IsActive = coupon_C_DTO.IsActive,
+        Name = coupon_C_DTO.Name,
+        Percent = coupon_C_DTO.Percent
+    };
+
     coupon.Id = CouponStore.couponList.Max(c => c.Id) + 1;
     CouponStore.couponList.Add(coupon);
-    return Results.CreatedAtRoute("GetCoupon",new { id = coupon.Id }, coupon);
-}).WithName("CreateCoupon").Accepts<Coupon>("application/json").Produces<Coupon>(200).Produces(400);
+    CouponDTO couponDTO = new()
+    {
+        Id = coupon.Id,
+        IsActive = coupon.IsActive,
+        Name = coupon.Name,
+        Percent = coupon.Percent,
+        Created = coupon.Created
+    };
+    return Results.CreatedAtRoute("GetCoupon",new { id = coupon.Id }, couponDTO);
+}).WithName("CreateCoupon").Accepts<CouponCreateDTO>("application/json").Produces<CouponDTO>(200).Produces(400);
 
 app.MapPut("/api/coupon/{id:int}", (int id) =>
 {
