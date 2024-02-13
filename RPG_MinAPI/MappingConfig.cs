c762b1f2f5b3e5b3c8761f559d54c47896958ffc
DanielO <11862549+Cardanont@users.noreply.github.com>
2024-02-12 23:50:42 -0600

    Hall Of Champions Added

---
+++ RPG_MinAPI/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RPG_MinAPI.Models;
+using RPG_MinAPI.Models.DTOs;
+
+namespace RPG_MinAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Champion, ChampionCreateDTO>().ReverseMap();
+        }
+    }
+}
--- RPG_MinAPI/Models/Champion.cs
+++ RPG_MinAPI/Models/Champion.cs
@@ -3,18 +3,20 @@
     public class Champion
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Class { get; set; }
+        public string? Name { get; set; }
+        public string? Class { get; set; }
         public double Level { get; set; }
         public double  Life { get; set; }
         public double Mana { get; set; }
-        public double Stamina { get; set; }
-        public double Strength { get; set; }
-        public double Dexterity { get; set; }
-        public double Intelligence { get; set; }
-        public double Wisdom { get; set; }
-        public double Charisma { get; set; }
+        public int Stamina { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
         public bool ItsAlive { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
 
     }
 }
